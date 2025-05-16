61a67fbeb85ec74d934208ef9fcd13787f704c4a
aman85662 <118710449+aman85662@users.noreply.github.com>
2025-05-16 12:06:33 +0530

    working on error

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,16 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes,Route} from "react-router";
+import {  Routes,Route} from "react-router";
 import './index.css'
 import Home from './views/Home.jsx'
 import Add from './views/Add.jsx'
 import EditStudents from './views/EditStudents.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
    <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/add" element={<Add />} />
     <Route path="/edit/:userId" element={<EditStudents />} />
    </Routes>
-  </BrowserRouter>
 )
